1ecc8213cd720b3f68f54a07ccf0944dc13ac123
Torsten-Fernandes <73098607+Torsten-Fernandes@users.noreply.github.com>
2020-10-24 12:54:01 +0530

    Add files via upload

---
+++ mod1-sol/app.js
@@ -0,0 +1,44 @@
+(function (){
+'use strict';
+
+angular.module('LunchCheck',[])
+
+.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject=['$scope']
+function LunchCheckController($scope)
+{
+	$scope.menuItems="";
+	$scope.response="";
+
+
+	function getCount(string){
+		var count=0;
+		var strarray=string.split(',');
+		for (var i = 0; i < strarray.length; i++) {
+			strarray[i] = strarray[i].replace(/ /g,'');
+        if(strarray[i].length >0) count++;
+      }
+      return count;
+		}
+	
+
+	$scope.checkIfTooMuch = function(){
+       switch (getMenuItemsCount($scope.menuItems)){
+         case 0:
+           $scope.response = 'Please enter data first';
+          
+           break;
+         case 1:
+         case 2:
+         case 3:
+           $scope.response = 'Enjoy!';
+          
+           break;
+         default:
+           $scope.response = 'Too much!';
+          
+       }
+    };
+
+}
+})();
